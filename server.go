d9a23bc4e2a71213aa66d087a78d79361313e241
Juan Boyce <juan@tvgla.com>
2016-07-10 15:29:26 -0400

    formatted code

--- server.go
+++ server.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"github.com/gorilla/websocket"
+	"log"
+	"math/rand"
 	"net/http"
 	"os"
-	"flag"
 	"strconv"
-	"log"
-	"github.com/gorilla/websocket"
-	"time"
-	"encoding/json"
-	"math/rand"
 	"sync"
+	"time"
 )
 
 const (
@@ -75,34 +75,34 @@ func (h *Hub) run() {
 
 //Factories are defined here
 type Factory struct {
-	Id int
-	Name string
-	Low int
-	High int
+	Id      int
+	Name    string
+	Low     int
+	High    int
 	Members []int
-	Delete bool
-	Count int
+	Delete  bool
+	Count   int
 }
 
 func getAllFactories() []Factory {
 	result := make([]Factory, 2)
-	result[0] = Factory {
-		Id: 69,
-		Name: "Jeremy",
-		Low: 70,
-		High: 420,
-		Members: []int {286,154},
-		Delete: false,
-		Count: 2,
+	result[0] = Factory{
+		Id:      69,
+		Name:    "Jeremy",
+		Low:     70,
+		High:    420,
+		Members: []int{286, 154},
+		Delete:  false,
+		Count:   2,
 	}
-	result[1] = Factory {
-		Id: 367,
-		Name: "djw",
-		Low: 42,
-		High: 256,
-		Members: []int {86,250,192},
-		Delete: false,
-		Count: 3,
+	result[1] = Factory{
+		Id:      367,
+		Name:    "djw",
+		Low:     42,
+		High:    256,
+		Members: []int{86, 250, 192},
+		Delete:  false,
+		Count:   3,
 	}
 	return result
 }
@@ -115,14 +115,14 @@ func getIdx(f []Factory, id int) int {
 			return idx
 		}
 	}
-	return -1;
+	return -1
 }
 
 func randInts(low int, high int, count int) []int {
 	spread := high - low + 1
 	result := make([]int, count)
 	for i := 0; i < count; i++ {
-		result[i] = low + rand.Int() % spread
+		result[i] = low + rand.Int()%spread
 	}
 	return result
 }
@@ -133,8 +133,8 @@ var (
 )
 
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 // Conn is an middleman between the websocket connection and the hub.
@@ -148,7 +148,7 @@ type Conn struct {
 
 // readPump pumps messages from the websocket connection to the hub.
 func (c *Conn) readPump() {
-    //close connection upon exit
+	//close connection upon exit
 	defer func() {
 		hub.unregister <- c
 		c.ws.Close()
@@ -157,8 +157,8 @@ func (c *Conn) readPump() {
 	c.ws.SetReadDeadline(time.Now().Add(pongWait))
 	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	//Setup infinite loop to read messages
-    for {
-	    _, buffer, err := c.ws.ReadMessage()
+	for {
+		_, buffer, err := c.ws.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v", err)
@@ -166,76 +166,76 @@ func (c *Conn) readPump() {
 			break
 		}
 
-	    //the incoming payload is a command
-	    //parse it and send the appropriate response
-	    command := string(buffer[:4])
-	    switch (command) {
-	    case "test":
-	    	//Only goes back to sender
-	    	c.send <- []byte("Hello World")
-	    case "list":
-	    	if data, err := json.Marshal(factories); err == nil {
-		    	c.send <- data	    		
-	    	}
-	    case "post":
-	    	//unmarsal json and upsert
-	    	newFactory := &Factory{}
-	    	id := 0
-	    	update := true
-	    	if err := json.Unmarshal(buffer[5:], newFactory); err == nil {
-	    		//if id is zero, add the factory to the array
-	    		if ( newFactory.Id == 0 ){
-	    			newFactory.Id = nextId.next()
-	    			factoryLock.Lock()
-	    			factories = append(factories, *newFactory)
-	    			factoryLock.Unlock()
-	    			update = false
-	    		}
-		    	id = newFactory.Id
-	    	}
-	    	//Yes, we are unmarshalling twice, once to get the id, again to update the record
+		//the incoming payload is a command
+		//parse it and send the appropriate response
+		command := string(buffer[:4])
+		switch command {
+		case "test":
+			//Only goes back to sender
+			c.send <- []byte("Hello World")
+		case "list":
+			if data, err := json.Marshal(factories); err == nil {
+				c.send <- data
+			}
+		case "post":
+			//unmarsal json and upsert
+			newFactory := &Factory{}
+			id := 0
+			update := true
+			if err := json.Unmarshal(buffer[5:], newFactory); err == nil {
+				//if id is zero, add the factory to the array
+				if newFactory.Id == 0 {
+					newFactory.Id = nextId.next()
+					factoryLock.Lock()
+					factories = append(factories, *newFactory)
+					factoryLock.Unlock()
+					update = false
+				}
+				id = newFactory.Id
+			}
+			//Yes, we are unmarshalling twice, once to get the id, again to update the record
 			factoryLock.Lock()
-    		if idx := getIdx(factories, id); idx >= 0 {
-    			if update {
+			if idx := getIdx(factories, id); idx >= 0 {
+				if update {
 					json.Unmarshal(buffer[5:], &factories[idx])
 				}
 				//Generate members if necessary
 				if newFactory.Count > 0 {
 					factories[idx].Members = randInts(factories[idx].Low, factories[idx].High, factories[idx].Count)
 				}
-				if data, err := json.Marshal(factories[idx:idx+1]); err == nil {
+				if data, err := json.Marshal(factories[idx : idx+1]); err == nil {
 					//New update goes to everyone
 					hub.broadcast <- data
-    			}
-    		}
+				}
+			}
 			factoryLock.Unlock()
-	   	case "retr":
-	    	//parse id and return single
-	    	if id, err := strconv.ParseInt(string(buffer[5:]), 10, 64); err == nil {
-	    		//now locate the record
-	    		if idx := getIdx(factories, int(id)); idx >= 0 {
-    				if data, err := json.Marshal(factories[idx:idx+1]); err == nil {
-    					c.send <- data
-	    			}
-	    		}
-	    	}
-	    case "dele":
-	    	//parse id
-	    	if id, err := strconv.ParseInt(string(buffer[5:]), 10, 64); err == nil {
-    			factoryLock.Lock()
-	    		if idx := getIdx(factories, int(id)); idx >= 0 {
-    				factories = append(factories[:idx], factories[idx+1:]...)
-    				result := make([]Factory, 1)
-    				result[0] = Factory{Id: int(id), Delete: true}
-    				if data, err := json.Marshal(result); err == nil {
-    					hub.broadcast <- data
-    				}	    			
-	    		}
-    			factoryLock.Unlock()
-	    	}
-	    case "echo":
-	    	c.send <- buffer[5:]
-	    }
+		case "retr":
+			//parse id and return single
+			if id, err := strconv.ParseInt(string(buffer[5:]), 10, 64); err == nil {
+				//now locate the record
+				if idx := getIdx(factories, int(id)); idx >= 0 {
+					if data, err := json.Marshal(factories[idx : idx+1]); err == nil {
+						c.send <- data
+					}
+				}
+			}
+		case "dele":
+			//parse id
+			if id, err := strconv.ParseInt(string(buffer[5:]), 10, 64); err == nil {
+				factoryLock.Lock()
+				if idx := getIdx(factories, int(id)); idx >= 0 {
+					factories = append(factories[:idx], factories[idx+1:]...)
+					result := make([]Factory, 1)
+					result[0] = Factory{Id: int(id), Delete: true}
+					if data, err := json.Marshal(result); err == nil {
+						hub.broadcast <- data
+					}
+				}
+				factoryLock.Unlock()
+			}
+		case "echo":
+			c.send <- buffer[5:]
+		}
 	}
 }
 
@@ -290,7 +290,7 @@ var factories = getAllFactories()
 var factoryLock sync.Mutex
 
 type IdCounter struct {
-	id int
+	id  int
 	mux sync.Mutex
 }
 
@@ -302,39 +302,36 @@ func (idc *IdCounter) next() int {
 	return val
 }
 
-var nextId = IdCounter { id: 401 }
-
+var nextId = IdCounter{id: 401}
 
 func main() {
 	var listenAddr = flag.String("http", ":8080", "address to listen on for HTTP")
 	flag.Parse()
+	file, _ := os.Open("index.html")
+	buffer := make([]byte, 100000)
+	contentLength, _ := file.Read(buffer)
+	file.Close()
 	go hub.run()
+
+	http.Handle("/ws", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		conn := &Conn{send: make(chan []byte, 256), ws: ws}
+		hub.register <- conn
+		go conn.writePump()
+		conn.readPump()
+	}))
+
 	//Serve index file
-	http.Handle("/test", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		file, _ := os.Open("index.html")
-		buffer := make([]byte,100000)
-		contentLength, _ := file.Read(buffer)
-		file.Close()
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("Date", "Sun, 08 May 2016 14:04:53 GMT")
 		w.Header().Set("Content-Length", strconv.Itoa(contentLength))
 		w.Write(buffer[:contentLength])
 	}))
-
-
-
-http.Handle("/ws", http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-	conn := &Conn{send: make(chan []byte, 256), ws: ws}
-	hub.register <- conn
-	go conn.writePump()
-	conn.readPump()
-}));
-
 
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
